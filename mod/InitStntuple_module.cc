0459bf654c3f2ae2f19e2248ff1da4718f00c1a2
matthewstortini <matthewstortini@gmail.com>
2025-04-04 23:07:25 -0500

    changing sequence name to reflect changes in mu2e-trig-config

--- mod/InitStntuple_module.cc
+++ mod/InitStntuple_module.cc
@@ -194,7 +194,7 @@ int InitStntuple::InitTriggerTable(int RunNumber) {
     trigger_table->AddTrigger(new TStnTrigger( 180, 180,"apr_highP_stopTarg"          ,1));
     trigger_table->AddTrigger(new TStnTrigger( 181, 181,"apr_highP"                   ,1));
     trigger_table->AddTrigger(new TStnTrigger( 190, 190,"apr_lowP_stopTarg"           ,1));
-    trigger_table->AddTrigger(new TStnTrigger( 191, 191,"apr_lowP_stopTarg_multiTrk"  ,1));
+    trigger_table->AddTrigger(new TStnTrigger( 191, 191,"apr_highP_stopTarg_multiTrk"  ,1));
     trigger_table->AddTrigger(new TStnTrigger( 195, 195,"aprHelix"                    ,1));
 
     trigger_table->AddTrigger(new TStnTrigger( 200, 200,"caloFast_photon"             ,1));
